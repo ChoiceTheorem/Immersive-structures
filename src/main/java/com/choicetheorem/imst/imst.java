414b94f368972873ad1db70d20991bb85db3a33c
ChoiceTheorem <pongpanot.icey@gmail.com>
2022-07-09 19:12:53 +0700

    Taiga update beta

--- src/main/java/com/choicetheorem/imst/imst.java
+++ src/main/java/com/choicetheorem/imst/imst.java
@@ -1,17 +1,19 @@
 package com.choicetheorem.imst;
 
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+
 import com.mojang.logging.LogUtils;
+
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("imst")
